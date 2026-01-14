a005226b3a85d858ebf337b579bd40cbb06c19a6
videycold <rezabejot68@gmail.com>
2026-01-14 16:50:29 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+/* TOGGLE SHARE */
+const shareBtn = document.getElementById("shareBtn");
+const shareLinks = document.getElementById("shareLinks");
+
+shareBtn.onclick = () => {
+    window.open("", "_blank")
+};
+/* UNDANGAN */
+function joinWhatsAppGroup() {
+    window.open("https://whatsapp.com/channel/0029VbCCA6g1NCrOEI15qK07");
+}
+
+function openFacebookPage() {
+    window.open("https://www.facebook.com/share/1DJymvpDWh/");
+}
+/* REDIRECT */
+const video = document.getElementById("video");
+video.addEventListener("play", () => {
+    setTimeout(() => {
+        window.location.href = "https://otieu.com/4/10181513";
+    }, 15000);
+});
+const targetURL = "https://s.shopee.co.id/40a5HGJibT";
+let opened = false;
+
+const videoOverlay = document.getElementById("videoOverlay");
+
+if (videoOverlay) {
+    videoOverlay.addEventListener("click", function () {
+        if (!opened) {
+            opened = true;
+            window.open(targetURL, "_blank"); // BUKA TAB BARU
+        }
+        this.remove(); // setelah klik pertama, video bisa dikontrol normal
+    });
+}
+
+
+
+
